81f7b6b79ae4523ae22092fede22457c69108d54
ReDeiPirati <alessio.gozzoli@gmail.com>
2018-06-22 19:18:37 +0200

    Add <Try it now> on top

--- support.py
+++ support.py
@@ -8,11 +8,18 @@ from keras.applications.inception_resnet_v2 import preprocess_input
 from keras.preprocessing import image
 from keras.preprocessing.image import img_to_array, load_img
 
+from keras.models import Sequential, Model
+from keras.engine import Layer
+from keras.layers import Conv2D, UpSampling2D, InputLayer, Conv2DTranspose, Input, Reshape, merge, concatenate, Activation, Dense, Dropout, Flatten
+from keras.layers.normalization import BatchNormalization
+from keras.layers.core import RepeatVector, Permute
+
 from skimage.color import rgb2lab, lab2rgb, rgb2gray, gray2rgb
 from skimage.transform import resize
 from skimage.io import imsave
 
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 #Create embedding
 def create_inception_embedding(inception, grayscaled_rgb):
@@ -135,3 +142,59 @@ def prediction_from_url(url, model, inception):
     ax3.axis('off')
     ax3.set_title('Original')
     ax3.imshow(read_img('test', '/', 'tmp', (256, 256))/255)
+
+    
+def load_pretrained_model(inception_wpath, colornet_wpath):
+    '''Load Emil's pretrained model'''
+    print('Loading pretrained model... (it could take a while)')
+    
+    #Load weights of InceptionResNet model for embedding extraction 
+    inception = InceptionResNetV2(weights=None, include_top=True)
+    inception.load_weights(inception_wpath)
+    inception.graph = tf.get_default_graph()
+    
+    # The Model
+    def conv_stack(data, filters, s):
+        """Utility for building conv layer"""
+        output = Conv2D(filters, (3, 3), strides=s, activation='relu', padding='same')(data)
+        return output
+
+    embed_input = Input(shape=(1000,))
+
+    #Encoder
+    encoder_input = Input(shape=(256, 256, 1,))
+    encoder_output = conv_stack(encoder_input, 64, 2)
+    encoder_output = conv_stack(encoder_output, 128, 1)
+    encoder_output = conv_stack(encoder_output, 128, 2)
+    encoder_output = conv_stack(encoder_output, 256, 1)
+    encoder_output = conv_stack(encoder_output, 256, 2)
+    encoder_output = conv_stack(encoder_output, 512, 1)
+    encoder_output = conv_stack(encoder_output, 512, 1)
+    encoder_output = conv_stack(encoder_output, 256, 1)
+
+    #Fusion
+    # y_mid: (None, 256, 28, 28)
+    fusion_output = RepeatVector(32 * 32)(embed_input) 
+    fusion_output = Reshape(([32, 32, 1000]))(fusion_output)
+    fusion_output = concatenate([encoder_output, fusion_output], axis=3) 
+    fusion_output = Conv2D(256, (1, 1), activation='relu')(fusion_output) 
+
+
+
+    #Decoder
+    decoder_output = conv_stack(fusion_output, 128, 1)
+    decoder_output = UpSampling2D((2, 2))(decoder_output)
+    decoder_output = conv_stack(decoder_output, 64, 1)
+    decoder_output = UpSampling2D((2, 2))(decoder_output)
+    decoder_output = conv_stack(decoder_output, 32, 1)
+    decoder_output = conv_stack(decoder_output, 16, 1)
+    decoder_output = Conv2D(2, (2, 2), activation='tanh', padding='same')(decoder_output)
+    decoder_output = UpSampling2D((2, 2))(decoder_output)
+
+    model = Model(inputs=[encoder_input, embed_input], outputs=decoder_output)
+    
+    # Load colornet weights
+    model.load_weights(colornet_wpath)
+    
+    print('Model loaded!')
+    return(model, inception)
